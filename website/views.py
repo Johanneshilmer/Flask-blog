71da92c3025fca30cfa7037575439b5b76844bcf
Johannes Hilmer <johanneshilmerson94@gmail.com>
2023-03-21 12:58:07 +0100

    delete-comment

--- website/views.py
+++ website/views.py
@@ -75,3 +75,19 @@ def create_comment(post_id):
             flash("Post does not exist!", category="error")
 
     return redirect(url_for("views.home"))
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash("Comment dose not exist!", category="error")
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash("You do not have premission to delete this comment.", category="error")
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for("views.home"))
+
